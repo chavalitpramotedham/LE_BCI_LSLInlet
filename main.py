99f97a5cc506f253871fe29fb50e82b3a4e677e2
chavalitpramotedham <chav0001@e.ntu.edu.sg>
2022-03-23 15:40:42 +0800

    final updates from lenovo

--- main.py
+++ main.py
@@ -4,166 +4,172 @@ from pylsl import StreamInlet, resolve_byprop, StreamInfo, StreamOutlet, local_c
 from time import time
 from random import random as rand
 
-def recordAndClassify(
-        duration: int,
-        dejitter=False,
-        continuous: bool = True,
-) -> None:
-    # constants:
-    LSL_EEG_CHUNK = 1
-    LSL_SCAN_TIMEOUT = 5
-    LSL_BUFFER = 360
+class LebciPython:
+    def __init__(self, duration):
+        self.duration = duration
 
-    OUT_STREAMNAME = "LE_BCI_DL_Output"
-    OUT_STREAMTYPE = "DL"
-    OUT_STREAMID = "LE_BCI_DL_Output"
 
-    # Initialization
-    chunk_length = LSL_EEG_CHUNK
+    def recordAndClassify(
+            self,
+            dejitter=False,
+            continuous: bool = True,
+    ) -> None:
+        # constants:
+        LSL_EEG_CHUNK = 1
+        LSL_SCAN_TIMEOUT = 5
+        LSL_BUFFER = 360
 
-    # A. BrainVision Stream
-    print("Looking for stream...")
-    streams = resolve_byprop('name', 'BrainVision RDA', timeout=LSL_SCAN_TIMEOUT)
+        OUT_STREAMNAME = "LE_BCI_DL_Output"
+        OUT_STREAMTYPE = "DL"
+        OUT_STREAMID = "LE_BCI_DL_Output"
 
-    if len(streams) == 0:
-        print("Can't find stream.")
-        return
+        # Initialization
+        chunk_length = LSL_EEG_CHUNK
 
-    print("Started acquiring data.")
-    inlet = StreamInlet(streams[0], max_chunklen=chunk_length)
-    # eeg_time_correction = inlet.time_correction()
+        # A. BrainVision Stream
+        print("Looking for stream...")
+        streams = resolve_byprop('name', 'BrainVision RDA', timeout=LSL_SCAN_TIMEOUT)
 
-    # B. Unity Marker Stream
-    print("Looking for a Markers stream...")
-    marker_streams = resolve_byprop(
-        'name', 'Unity.LEBCI_Stream', timeout=LSL_SCAN_TIMEOUT)
+        if len(streams) == 0:
+            print("Can't find stream.")
+            return
 
-    if marker_streams:
-        inlet_marker = StreamInlet(marker_streams[0])
-        print("Found Markers stream")
-    else:
-        inlet_marker = False
-        print("Can't find Markers stream.")
-        return
+        print("Started acquiring data.")
+        inlet = StreamInlet(streams[0], max_chunklen=chunk_length)
+        # eeg_time_correction = inlet.time_correction()
 
-    in_info = inlet.info()
-    description = in_info.desc()
+        # B. Unity Marker Stream
+        print("Looking for a Markers stream...")
+        marker_streams = resolve_byprop(
+            'name', 'Unity.LEBCI_Stream', timeout=LSL_SCAN_TIMEOUT)
 
-    Nchan = in_info.channel_count()
+        if marker_streams:
+            inlet_marker = StreamInlet(marker_streams[0])
+            print("Found Markers stream")
+        else:
+            inlet_marker = False
+            print("Can't find Markers stream.")
+            return
 
-    ch = description.child('channels').first_child()
-    ch_names = [ch.child_value('label')]
-    for i in range(1, Nchan):
-        ch = ch.next_sibling()
-        ch_names.append(ch.child_value('label'))
+        in_info = inlet.info()
+        description = in_info.desc()
 
-    # C. Classification Output Stream
-    out_info = StreamInfo(OUT_STREAMNAME, OUT_STREAMTYPE, 1, 0.0, 'float32', OUT_STREAMID)
-    outlet = StreamOutlet(out_info)
+        Nchan = in_info.channel_count()
 
-    res = []
-    timestamps = []
-    markers = []
+        ch = description.child('channels').first_child()
+        ch_names = [ch.child_value('label')]
+        for i in range(1, Nchan):
+            ch = ch.next_sibling()
+            ch_names.append(ch.child_value('label'))
 
-    currentSample = []
-    inBCI = False
+        # C. Classification Output Stream
+        out_info = StreamInfo(OUT_STREAMNAME, OUT_STREAMTYPE, 1, 0.0, 'float32', OUT_STREAMID)
+        outlet = StreamOutlet(out_info)
 
-    t_init = time()
-    time_correction = inlet.time_correction()
-    last_written_timestamp = None
+        res = []
+        timestamps = []
+        markers = []
 
-    print('Start recording at time t=%.3f' % t_init)
-    print('Time correction: ', time_correction)
-    while (time() - t_init) < duration:
-        try:
+        currentSample = []
+        inBCI = False
 
-            # 1. Check for data and append to res & timestamp arrays
+        t_init = time()
+        time_correction = inlet.time_correction()
+        last_written_timestamp = None
 
-            data, timestamp_d = inlet.pull_chunk(
-                timeout=1.0, max_samples=chunk_length)
+        print('Start recording at time t=%.3f' % t_init)
+        print('Time correction: ', time_correction)
+        while (time() - t_init) < self.duration:
+            try:
 
-            # #debug
-            # if (data):
-            #     print("DATA: "+str(data))
-            # if (timestamp_d):
-            #     print("DATA TIMESTAMP: "+str(timestamp_d))
+                # 1. Check for data and append to res & timestamp arrays
 
-            if timestamp_d:
-                res.append(data)
-                timestamps.extend(timestamp_d)
-                tr = time()
-
-                if (inBCI):
-                    currentSample.append((data,timestamp_d))
-
-            # 2. Check for markers and append to markers array
-
-            if inlet_marker:
-                marker, timestamp_m = inlet_marker.pull_sample(timeout=0.0)
+                data, timestamp_d = inlet.pull_chunk(
+                    timeout=1.0, max_samples=chunk_length)
 
                 # #debug
-                # if (marker):
-                #     print("MARKER: "+str(marker))
-                # if (timestamp_m):
-                #     print("MARKER TIMESTAMP: "+str(timestamp_m))
+                # if (data):
+                #     print("DATA: "+str(data))
+                # if (timestamp_d):
+                #     print("DATA TIMESTAMP: "+str(timestamp_d))
 
-                if timestamp_m:
-                    markers.append([marker, timestamp_m])
+                if timestamp_d:
+                    res.append(data)
+                    timestamps.extend(timestamp_d)
+                    tr = time()
 
-                    if int(marker[0]) == 102:
-                        print("BCI START")
-                        currentSample = []
-                        inBCI = True
+                    if (inBCI):
+                        currentSample.append((data,timestamp_d))
 
-                    elif int(marker[0]) == 101 and inBCI:
-                        print("BCI END")
-                        inBCI = False
-                        print(currentSample)
+                # 2. Check for markers and append to markers array
 
-                        res = [classify(currentSample)]
-                        print("RESULT = "+str(res))
+                if inlet_marker:
+                    marker, timestamp_m = inlet_marker.pull_sample(timeout=0.0)
 
-                        outlet.push_sample(res)
+                    # #debug
+                    # if (marker):
+                    #     print("MARKER: "+str(marker))
+                    # if (timestamp_m):
+                    #     print("MARKER TIMESTAMP: "+str(timestamp_m))
 
-            # # Save every 5s
-            # if continuous and (last_written_timestamp is None or last_written_timestamp + 5 < timestamps[-1]):
-            #     _save(
-            #         filename,
-            #         res,
-            #         timestamps,
-            #         time_correction,
-            #         dejitter,
-            #         inlet_marker,
-            #         markers,
-            #         ch_names,
-            #         last_written_timestamp=last_written_timestamp,
-            #     )
-            #     last_written_timestamp = timestamps[-1]
+                    if timestamp_m:
+                        markers.append([marker, timestamp_m])
 
-        except KeyboardInterrupt:
-            break
+                        if int(marker[0]) == 102:
+                            print("BCI START")
+                            currentSample = []
+                            inBCI = True
 
-    time_correction = inlet.time_correction()
-    print("Time correction: ", time_correction)
+                        elif int(marker[0]) == 101 and inBCI:
+                            print("BCI END")
+                            inBCI = False
+                            print(currentSample)
 
-    # _save(
-    #     filename,
-    #     res,
-    #     timestamps,
-    #     time_correction,
-    #     dejitter,
-    #     inlet_marker,
-    #     markers,
-    #     ch_names,
-    # )
-    #
-    # print("Done - wrote file: {}".format(filename))
+                            res = [self.classify(currentSample)]
+                            print("RESULT = "+str(res))
 
-def classify(currentSample):
+                            outlet.push_sample(res)
 
-    res = random.choice([0, 1])
-    return res
+                # # Save every 5s
+                # if continuous and (last_written_timestamp is None or last_written_timestamp + 5 < timestamps[-1]):
+                #     _save(
+                #         filename,
+                #         res,
+                #         timestamps,
+                #         time_correction,
+                #         dejitter,
+                #         inlet_marker,
+                #         markers,
+                #         ch_names,
+                #         last_written_timestamp=last_written_timestamp,
+                #     )
+                #     last_written_timestamp = timestamps[-1]
+
+            except KeyboardInterrupt:
+                break
+
+        time_correction = inlet.time_correction()
+        print("Time correction: ", time_correction)
+
+        # _save(
+        #     filename,
+        #     res,
+        #     timestamps,
+        #     time_correction,
+        #     dejitter,
+        #     inlet_marker,
+        #     markers,
+        #     ch_names,
+        # )
+        #
+        # print("Done - wrote file: {}".format(filename))
+
+    def classify(currentSample):
+
+        res = random.choice([0, 1])
+        return res
 
 
 if __name__ == "__main__":
-    recordAndClassify(1000)
+    py = LebciPython(1000)
+    py.recordAndClassify()
